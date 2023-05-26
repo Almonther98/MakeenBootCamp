5ee9fb9bad1e427cfb06aef41b7500314ee44cb8
Almundhar <mxa.musa@gmail.com>
2023-05-26 20:54:45 +0400

    advance C# topic (1)

---
+++ C#_learning_journey/book_store_using_ Indexer/FavoriteBook.cs
@@ -0,0 +1,150 @@
+﻿using System.Globalization;
+
+namespace favoriteBook
+{
+    internal class FavoriteBook
+        {
+            private string[] names;
+            private string[] authors;
+        
+
+
+
+            // constracter 
+            public FavoriteBook(int size)
+            {
+                
+                this.names = new string[size];
+                this.authors = new string[size];
+            }
+
+
+            // set 
+            public string this[int idx, string name]
+            {
+                set { this.names[idx] = name; this.authors[idx] = value; }
+
+            }
+            // get by name
+            public string this[string name]
+            {
+                get
+                {
+                    for (int i = 0; i < names.Length; i++)
+                    {
+                        if (this.names[i] == name) return "Name: " + this.names[i] + "  Author: " + this.authors[i];
+                    }
+
+                    return "notfound !";
+                }
+            }
+
+            // by index
+            public string this[int index]
+            {
+                get
+                {
+                    return $"Name: {this.names[index]} --> Author: {this.authors[index]}";
+                }
+            }
+
+
+            public void DisplayAllBooks()
+            {
+                Console.WriteLine("******************BOOKS******************");
+                for (int i = 0; i < names.Length; i++)
+                {
+                    Console.WriteLine($"BookNumber [ {i + 1} ] Name: {names[i]}  ");
+                }
+                Console.WriteLine("******************BOOKS******************");
+
+            }
+            /*
+             
+             ##### this function delete the book name and the author from the tow lists by the book number 
+                    input: book number 
+             
+             
+             */
+            public void barrowBook(int pos)
+            {
+                
+                int indx = 0;
+
+                string[] tempNames = new string[names.Length - 1];
+                string[] tempAuthors = new string[authors.Length - 1];
+
+                for (int i = 0; i < names.Length; i++)
+                {
+
+                    if (i == pos - 1)
+                    {
+                        continue;
+                    }
+
+                    tempNames[indx] = names[i];
+                    tempAuthors[indx] = authors[i];
+
+                    indx++;
+
+                }
+
+                names = tempNames;
+                authors = tempAuthors;
+            }
+            /*
+             
+             ##### this function add the book name and the author to the tow lists by taking book name and the book author 
+                    input: -book name 
+                           -author name 
+             
+             
+             */
+        public void returnBook(string name, string aouthr)
+            {
+
+                if (names == null)
+                {
+                    names = new string[] { name };
+                    authors = new string[] { aouthr };
+                }
+                else
+                {
+                    string[] tempNames = new string[names.Length + 1];
+                    string[] tempAuthors = new string[authors.Length + 1];
+
+                    for (int i = 0; i < names.Length; i++)
+                    {
+                        tempNames[i] = names[i];
+                        tempAuthors[i] = authors[i];
+                    }
+                    tempNames[tempNames.Length - 1] = name;
+                    tempAuthors[tempAuthors.Length - 1] = aouthr;
+                    authors = tempAuthors;
+                    names = tempNames;
+
+                }
+            }
+
+            /*
+              this function return the size of the nams list as the length of both lists length 
+             */
+            public int getSize()
+            {
+                return names.Length;
+            }
+
+            // grt book info
+            public string getBookName(int index)
+            {
+                return names[index-1];
+            }
+            public string getBookAuthor(int index)
+            {
+                return authors[index-1];
+            }
+
+
+    }
+
+    }
---
+++ C#_learning_journey/book_store_using_ Indexer/Program.cs
@@ -0,0 +1,163 @@
+﻿namespace favoriteBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // create instenc 
+            FavoriteBook booklist = new FavoriteBook(5);
+            // initial  books 
+            booklist[0, "python"] = "ahmed mohsen";
+            booklist[1, "C#"] = "ahmed ";
+            booklist[2, "java"] = "ahmed mhammed ";
+            booklist[3, "flutter"] = "ahmed ali";
+            booklist[4, "C"] = "ahmed jeo";
+
+            // create barrowed books list
+            FavoriteBook barrowedbooklist = new FavoriteBook(0);
+
+
+            // this loop to take the user option to barrow book or return  book or quit the program 
+            while (true)
+            {
+
+                Console.WriteLine("Enter ( 1 ) to barrow  books , ( 2 ) to return a book. **( 0 )** to End Program.");
+                int mainOption = 0; 
+                while (true)
+                {
+                    if (int.TryParse(Console.ReadLine(), out int number))
+                    {
+                        if (number == 1 || number ==2 || number ==0)
+                        {
+                            mainOption = number;
+                            break;
+                        }
+
+                    }
+                    Console.WriteLine("invalid input !!");
+                    Console.WriteLine();
+                    Console.WriteLine("Enter ( 1 ) to barrow  books , ( 2 ) to return a book. **( 0 )** to End Program.");
+                }
+                if (mainOption == 0) { break; }
+
+                // baroow opration
+                if(mainOption == 1)
+                {
+                    Console.WriteLine("Books to barrow:");
+                    booklist.DisplayAllBooks();
+                    Console.WriteLine();
+                    Console.WriteLine("Select a book  by enter the book number -- enter ( 0 ) to out the program. :");
+                    int bookNumber;
+                    // this loop to take the nuber of the book to barrow 
+                    while (true)
+                    {
+                        if (int.TryParse(Console.ReadLine(), out int number))
+                        {
+                            if (number >= 0 && number <=booklist.getSize())
+                            {
+                                bookNumber = number;
+                                break;
+                            }
+
+                        }
+                        Console.WriteLine("invalid Book Number !!");
+                    }
+
+                    if (bookNumber == 0) { break; }
+
+                    Console.WriteLine();
+                    Console.WriteLine($"Book Information");
+                    Console.WriteLine(booklist[bookNumber-1]);
+                    Console.WriteLine();
+
+                    Console.WriteLine(" select ( 1 ) to barrow , ( 0 ) to end shoping");
+
+                    int option;
+                    // this loop to conferm the barrow 
+                    while (true)
+                    {
+                        if (int.TryParse(Console.ReadLine(), out int number))
+                        {
+                            if (number == 0 || number == 1)
+                            {
+                                option = number;
+                                break;
+                            }
+
+                        }
+                        Console.WriteLine("invalid Opration Number !!");
+
+
+                    }
+                    if (option == 0) { break; }
+
+                    barrowedbooklist.returnBook(booklist.getBookName(bookNumber), booklist.getBookAuthor(bookNumber));
+                    booklist.barrowBook(bookNumber);             
+                }
+
+                // return opration
+                if(mainOption == 2) 
+                {
+                    Console.WriteLine("Books to return:");
+                    barrowedbooklist.DisplayAllBooks();
+                    Console.WriteLine();
+                    Console.WriteLine("Select a book  by enter the book number -- enter ( 0 ) to out the program. :");
+                    int bookNumber;
+                    // this loop to take the nuber of the book to return 
+                    while (true)
+                    {
+                        if (int.TryParse(Console.ReadLine(), out int number))
+                        {
+                            
+                            if (number >= 0 && number <=barrowedbooklist.getSize())
+                            {
+                                bookNumber = number;
+                                break;
+                            }
+
+                        }
+                        Console.WriteLine("invalid Book Number !!");
+                    }
+
+                    if (bookNumber == 0) { break; }
+
+                    Console.WriteLine();
+                    Console.WriteLine($"Book Information");
+                    Console.WriteLine(barrowedbooklist[bookNumber-1]);
+                    Console.WriteLine();
+
+                    Console.WriteLine(" select ( 1 ) to return , ( 0 ) to end shoping");
+
+                    int option;
+                    // this loop to conferm the barrow 
+                    while (true)
+                    {
+                        if (int.TryParse(Console.ReadLine(), out int number))
+                        {
+                            if (number == 0 || number == 1)
+                            {
+                                option = number;
+                                break;
+                            }
+
+                        }
+                        Console.WriteLine("invalid Opration Number !!");
+
+
+                    }
+                    if (option == 0) { break; }
+
+                    booklist.returnBook(barrowedbooklist.getBookName(bookNumber), barrowedbooklist.getBookAuthor(bookNumber));
+                    barrowedbooklist.barrowBook(bookNumber);
+                }
+
+
+
+
+            }
+
+        }
+
+    }
+
+    }
